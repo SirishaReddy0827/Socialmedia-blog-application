044688d710f0d1174c985d755f17eecad306ce35
SirishaReddy0827 <sirisha.thippana27@gmail.com>
2024-06-19 15:48:21 -0700

    Initial commit for socialmedia blog apllication

---
+++ src/test/java/com/java/springboot/learning/socialmedia_blog_application/SocialmediaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springboot.learning.socialmedia_blog_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
